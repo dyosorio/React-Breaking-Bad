7462c454dd61af7cbe3eff27e1a66ef0824106af
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-11-20 19:13:48 +0100

    Finished character components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,33 @@
+ import React, { useState, useEffect } from 'react';
  import './App.css';
+ import Header from './components/ui/Header'
+ import CharacterGrid from './components/characters/CharacterGrid'
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+            fetch(`https://www.breakingbadapi.com/api/characters`)
+            .then(res => res.json())
+            .then(data => {
+            console.log(data)
+            setItems(data)
+            setIsLoading(false)
+        })
+        .catch(error => {
+            setError(true);
+        })
+    }
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
-      Hello 
+      <Header /> 
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
